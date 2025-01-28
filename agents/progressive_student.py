42b33be311e12f42453f7753e5e1a1114bc603ea
graduaition <bycocoaz@gmail.com>
2025-01-28 18:48:03 +0100

    Create progressive_student.py

---
+++ agents/progressive_student.py
@@ -0,0 +1,16 @@
+from services.twitter_client import TwitterClient
+from services.llm_client import LLMClient
+
+class ProgressiveStudent:
+    def __init__(self, name, twitter_client, llm_client):
+        self.name = name
+        self.twitter_client = twitter_client
+        self.llm_client = llm_client
+
+    def respond_to_problem(self, tweet_id):
+        response = self._generate_response()
+        self.twitter_client.reply_to_tweet(tweet_id, response)
+
+    def _generate_response(self):
+        ...
+        return self.llm_client.generate_text(prompt)
