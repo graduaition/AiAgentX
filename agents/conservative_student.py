ac5e17646abc2d8d9a98a219e06d7fe280fce129
graduaition <bycocoaz@gmail.com>
2025-01-28 17:28:05 +0100

    Create conservative_student.py

---
+++ agents/conservative_student.py
@@ -0,0 +1,17 @@
+from services.twitter_client import TwitterClient
+from services.llm_client import LLMClient
+
+class ConservativeStudent:
+    def __init__(self, name, twitter_client, llm_client):
+        self.name = name
+        self.twitter_client = twitter_client
+        self.llm_client = llm_client
+
+    def respond_to_problem(self, tweet_id):
+        response = self._generate_response()
+        self.twitter_client.reply_to_tweet(tweet_id, response)
+
+    def _generate_response(self):
+        """Generate a conservative answer (private)."""
+        ...
+        return self.llm_client.generate_text(prompt)
